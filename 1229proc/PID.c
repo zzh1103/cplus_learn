6e8e443eddfa69d59bc0f342e710f1a0cdb6f9db
zzh <1922732414@qq.com>
2025-12-30 09:16:23 +0800

    进程相关学习，fork等

---
+++ 1229proc/PID.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+
+    printf("pid: %d\n",getpid());
+    printf("ppid: %d\n",getppid());
+
+    return 0;
+}
+
---
+++ 1229proc/proc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    while(1)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
+
